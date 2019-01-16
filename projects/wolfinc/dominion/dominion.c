a901a554e1e567d1834f4442824a991a09b29d2a
oregonstatevr <osu.virtualreality@gmail.com>
2019-01-16 14:23:45 -0800

    Added Smithy Function

--- projects/wolfinc/dominion/dominion.c
+++ projects/wolfinc/dominion/dominion.c
@@ -643,6 +643,17 @@ int getCost(int cardNumber)
   return -1;
 }
 
+smithyFunction(int handPos, int currentPlayer, struct gameState state, int num)
+{
+  for (i = 0; i < 3; i++)
+  {
+    drawCard(currentPlayer, state);
+  }
+  //discard card from hand
+  discardCard(handPos, currentPlayer, state, 0);
+  return 0;
+}			
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -830,14 +841,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
     case smithy:
       //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+      smithyFunction(handPos, currentPlayer, state,0);
 		
     case village:
       //+1 Card
