88ea74b5d552d81ed6aead0bf1bed75c4133a02b
Conor Wolfin <wolfinc@flip2.engr.oregonstate.edu>
2019-02-10 19:36:33 -0800

    Quiz

--- projects/wolfinc/quiz/testme.c
+++ projects/wolfinc/quiz/testme.c
@@ -3,16 +3,48 @@
 #include<stdlib.h>
 #include<time.h>
 
-char inputChar()
+char inputChar(int state)
 {
-    // TODO: rewrite this function
-    return ' ';
+	 // 50:50 chance between correct and incorrect next char
+	 char randChar = ' ';
+	 switch(state)
+	 {
+	 case 0: case 8:
+		 randChar = rand() % 3 + ((int)'[');	
+	   break;
+	 case 1: case 7:
+		 randChar = rand() % 3 + ((int)'(');
+	   break;
+	 case 2: case 6:
+		 randChar = rand() % 3 + ((int)'{');
+	   break;
+	 case 3:
+		 randChar = rand() % 3 + ((int)' ');
+	   break;
+	 case 4:
+		 randChar = rand() % 3 + ((int)'a');
+	   break;
+	 case 5:
+		 randChar = rand() % 3 + ((int)'x');
+	   break;
+	 default:
+		 randChar = ' ';
+	 }
+	 return randChar;
 }
 
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+	int i = 0;
+	char string[6];
+	char *options = "reset\0"; 
+  for(i; i < 6 ; i++)
+	{ 
+		string[i] = options[rand() % 6];
+	}
+	char *finalStr;
+	finalStr = string;
+	return finalStr;
 }
 
 void testme()
@@ -24,14 +56,14 @@ void testme()
   while (1)
   {
     tcCount++;
-    c = inputChar();
+    c = inputChar(state);
     s = inputString();
     printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
 
     if (c == '[' && state == 0) state = 1;
     if (c == '(' && state == 1) state = 2;
     if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
+    if (c == ' ' && state == 3) state = 4;
     if (c == 'a' && state == 4) state = 5;
     if (c == 'x' && state == 5) state = 6;
     if (c == '}' && state == 6) state = 7;
@@ -42,7 +74,7 @@ void testme()
        && s[4] == 't' && s[5] == '\0'
        && state == 9)
     {
-      printf("error ");
+      printf("error \n");
       exit(200);
     }
   }
